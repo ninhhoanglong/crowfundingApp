bbdd272ad388e5855fd0b352de8e0afed58c4c1f
Long <ninhhoanglong20@gmail.com>
2022-10-11 11:52:04 +0700

    Code Component Search,Overlay and Search modal box

--- src/modules/dashboard/DashboardSearch.js
+++ src/modules/dashboard/DashboardSearch.js
@@ -1,33 +1,104 @@
+import { defaultImage } from "constants/global";
 import React from "react";
+import { useState } from "react";
 
 const DashboardSearch = () => {
+  const [showSearch, setShowSearch] = useState(false);
   return (
-    <div className="bg-white rounded-full shadow-[10px_10px_20px_rgba(218,_213,_213,_0.15)] p-2 w-full flex items-center">
-      <div className="flex-1 px-5">
-        <input
-          type="text"
-          placeholder="Do fundrise now"
-          className="w-full text-sm bg-transparent placeholder:text-text4 text-text1"
-        />
-      </div>
-      <button className="w-[72px] rounded-full bg-primary text-white h-10 flex items-center justify-center flex-shrink-0">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+    <div className="relative z-50">
+      <div className="bg-white rounded-full shadow-[10px_10px_20px_rgba(218,_213,_213,_0.15)] p-2 w-full flex items-center">
+        <div className="flex-1 px-5">
+          <input
+            type="text"
+            placeholder="Do fundrise now"
+            className="w-full text-sm bg-transparent placeholder:text-text4 text-text1"
           />
-        </svg>
-      </button>
+        </div>
+        <button className="w-[72px] rounded-full bg-primary text-white h-10 flex items-center justify-center flex-shrink-0">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
+        </button>
+      </div>
+      {showSearch && (
+        <div className="search-results w-full lg:w-[843px] bg-white absolute top-full left-0 z-50 translate-y-5 pb-6 rounded-3xl">
+          <div className="flex items-center justify-between p-3 bg-graySoft rounded-3xl">
+            <h4 className="pl-4 text-sm font-medium underline">
+              See all 10,124 fundraisier
+            </h4>
+            <button className="flex items-center justify-center w-[72px] h-[50px] rounded-xl bg-error bg-opacity-20 text-error">
+              <svg
+                width={24}
+                height={24}
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g clipPath="url(#clip0_2253_7016)">
+                  <path
+                    d="M20 5.61143L18.3886 4L12 10.3886L5.61143 4L4 5.61143L10.3886 12L4 18.3886L5.61143 20L12 13.6114L18.3886 20L20 18.3886L13.6114 12L20 5.61143Z"
+                    fill="#EB5757"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0_2253_7016">
+                    <rect width={24} height={24} fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
+            </button>
+          </div>
+          <div className="p-6 pb-0">
+            <div className="flex flex-col mb-6 gap-y-5">
+              <SearchItem></SearchItem>
+              <SearchItem></SearchItem>
+              <SearchItem></SearchItem>
+              <SearchItem></SearchItem>
+            </div>
+            <h3 className="mb-4 text-sm font-semibold">Related searchs</h3>
+            <div className="flex flex-col text-sm gap-y-3 text-text2">
+              <p>
+                <strong>education</strong> fund
+              </p>
+              <p>schoralship fund</p>
+              <p>
+                <strong>education</strong> and schools fund
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
+
+function SearchItem() {
+  return (
+    <div className="flex items-center gap-x-5">
+      <img
+        src={defaultImage}
+        alt=""
+        className="w-[50px] h-[50px] rounded-lg object-cover"
+      />
+      <div className="flex-1 text-sm">
+        <h3 className="mb-1">
+          <strong>Education</strong> fund for Durgham Family
+        </h3>
+        <p className="text-text3">By Durgham Family</p>
+      </div>
+    </div>
+  );
+}
 
 export default DashboardSearch;
